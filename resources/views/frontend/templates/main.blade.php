5f54854fe40dd643fe7136216944626113bddc7a
DenisBashkirov <era-digital@yandex.ru>
2019-05-01 22:11:06 +0300

    commit

--- resources/views/frontend/templates/main.blade.php
+++ resources/views/frontend/templates/main.blade.php
@@ -10,6 +10,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aliro</title>
+
+    @include('frontend.sources.styles')
+
 </head>
 <body>
 
