68961431980726e83052457da04ea87f1f90414a
DenisBashkirov <era-digital@yandex.ru>
2019-05-01 22:20:29 +0300

    commit

--- resources/views/frontend/sections/features.blade.php
+++ resources/views/frontend/sections/features.blade.php
@@ -1,15 +1,17 @@
 <div class="features-section">
+    <div class="container">
 
-    <div class="feature-item">
-        <div class="feature-item__icon">
-            <img src="{{ asset('svg/features/window.svg') }}" alt="">
+        <div class="feature-item">
+            <div class="feature-item__icon">
+                <img src="{{ asset('svg/features/window.svg') }}" alt="">
+            </div>
+            <p class="feature-item__title">
+                Напрямую от производителя
+            </p>
+            <p class="feature-item__text">
+                Производство, доставка и монтаж светопрозрачных конструкций любой сложности
+            </p>
         </div>
-        <p class="feature-item__title">
-            Напрямую от производителя
-        </p>
-        <p class="feature-item__text">
-            Производство, доставка и монтаж светопрозрачных конструкций любой сложности
-        </p>
-    </div>
 
+    </div>
 </div>
