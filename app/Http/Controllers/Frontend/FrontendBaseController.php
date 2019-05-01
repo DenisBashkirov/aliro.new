40536b92dccc0109308f517c1ffcdd2d41bb0b18
DenisBashkirov <era-digital@yandex.ru>
2019-05-01 21:34:05 +0300

    Update FrontendBaseController.php

--- app/Http/Controllers/Frontend/FrontendBaseController.php
+++ app/Http/Controllers/Frontend/FrontendBaseController.php
@@ -8,24 +8,8 @@ use Illuminate\Support\Arr;
 
 class FrontendBaseController extends BaseController
 {
-    protected $header_bg;
-    protected $header_unsticky;
-
     public function renderOutput()
     {
-        $this->vars = Arr::add($this->vars, 'header_bg', $this->header_bg);
-        $this->vars = Arr::add($this->vars, 'header_unsticky', $this->header_unsticky);
-        $this->vars = Arr::add($this->vars, 'page_title', $this->page_title);
-        $this->vars = Arr::add($this->vars, 'meta_description', $this->meta_description);
-        $this->vars = Arr::add($this->vars, 'meta_keywords', $this->meta_keywords);
-
         return parent::renderOutput();
-    }
-
-    public function get_meta($instance)
-    {
-        $this->page_title = $instance->title;
-        $this->meta_description = $instance->meta_description;
-        $this->meta_keywords = $instance->meta_keywords;
     }
 }
