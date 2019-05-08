6980528c991026451109f5375f2ff02f9b5d1ba0
DenisBashkirov <era-digital@yandex.ru>
2019-05-08 22:35:30 +0300

    commit

--- resources/views/frontend/sections/features.blade.php
+++ resources/views/frontend/sections/features.blade.php
@@ -1,6 +1,8 @@
 <div class="features-section">
     <div class="container">
 
+        <h2 class="h2 h2_small align-center">Преимущества</h2>
+
         <div class="features-section__items-grid">
 
             <div class="feature-item">
@@ -8,7 +10,7 @@
                     <img src="{{ asset('svg/features/window.svg') }}" alt="">
                 </div>
                 <div class="feature-item__article">
-                    <p class="feature-item__title">Напрямую от производителя</p>
+                    <p class="h3 h3_mini feature-item__title">Напрямую от производителя</p>
                     <p class="feature-item__text text_secondary-1 js-hyphen">Производство, доставка и монтаж светопрозрачных конструкций любой сложности</p>
                 </div>
             </div>
@@ -18,7 +20,7 @@
                     <img src="{{ asset('svg/features/ark.svg') }}" alt="">
                 </div>
                 <div class="feature-item__article">
-                    <p class="feature-item__title">Нестандартные изделия</p>
+                    <p class="h3 h3_mini feature-item__title">Нестандартные изделия</p>
                     <p class="feature-item__text text_secondary-1 js-hyphen">Фигурные, цветные, раздвижные, автоматические системы остекления</p>
                 </div>
             </div>
@@ -28,7 +30,7 @@
                     <img src="{{ asset('svg/features/region.svg') }}" alt="">
                 </div>
                 <div class="feature-item__article">
-                    <p class="feature-item__title">Краснодарский край и Адыгея</p>
+                    <p class="h3 h3_mini feature-item__title">Краснодарский край и Адыгея</p>
                     <p class="feature-item__text text_secondary-1 js-hyphen">Доставка и монтажные работы по всему Краснодарскому краю и республике Адыгея</p>
                 </div>
             </div>
