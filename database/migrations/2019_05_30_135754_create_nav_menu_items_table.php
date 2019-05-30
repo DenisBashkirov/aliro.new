ffa91db2899f79b7da0671ffbcb3685a0eef9cd3
DenisBashkirov <era-digital@yandex.ru>
2019-05-30 17:05:28 +0300

    commit

---
+++ database/migrations/2019_05_30_135754_create_nav_menu_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNavMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('nav_menu_items'))
+        {
+            Schema::create('nav_menu_items', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('text')->unique();
+                $table->bigInteger('page_id')->unsigned()->nullable();
+                $table->bigInteger('ordering');
+
+                $table->foreign('page_id')->references('id')->on('pages')->onDelete('restrict')->onUpdate('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nav_menu_items');
+    }
+}
---
+++ database/migrations/2019_05_30_140039_create_nav_dropdown_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNavDropdownItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('nav_dropdown_items'))
+        {
+            Schema::create('nav_dropdown_items', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('text');
+                $table->bigInteger('page_id')->unsigned()->nullable();
+                $table->bigInteger('parent_id')->unsigned()->nullable();
+                $table->bigInteger('ordering');
+
+                $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade')->onUpdate('cascade');
+                $table->foreign('parent_id')->references('id')->on('nav_dropdown_items')->onDelete('restrict')->onUpdate('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nav_dropdown_items');
+    }
+}
