77689d5d12e515698084164015949c0eed130b69
DenisBashkirov <era-digital@yandex.ru>
2019-05-31 12:22:19 +0300

    commit

--- resources/views/frontend/sections/header_test.blade.php
+++ resources/views/frontend/sections/header_test.blade.php
@@ -9,130 +9,6 @@
 
         <div class="header__menu">
             <nav class="nav">
-<!--
-                <div class="nav__item">
-                    <div>Продукция</div>
-                    <div class="nav__item-arrow"><span class="icon-arrow"></span></div>
-
-                    <div class="nav__dropdown">
-                        <div class="nav__dropdown-body nav__dropdown-grid">
-                            <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
-                                <span class="icon-arrow"></span>
-                                <span>назад</span>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Окна</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Пластиковые</a>
-                                    <a href="#" class="nav__dropdown-item">Алюминиевые</a>
-                                    <a href="#" class="nav__dropdown-item">Раздвижные</a>
-                                    <a href="#" class="nav__dropdown-item">Энергосберегающие</a>
-                                </div>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Двери</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Пластиковые</a>
-                                    <a href="#" class="nav__dropdown-item">Алюминиевые</a>
-                                    <a href="#" class="nav__dropdown-item">Раздвижные</a>
-                                </div>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Балконы и лоджии</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Пластиковые</a>
-                                    <a href="#" class="nav__dropdown-item">Алюминиевые</a>
-                                    <a href="#" class="nav__dropdown-item">Раздвижные</a>
-                                </div>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Конструкции</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Алюминиевые конструкции</a>
-                                    <a href="#" class="nav__dropdown-item">Перегородки</a>
-                                </div>
-                            </div>
-                            <div class="nav__category nav__category_big">
-                                <div class="nav__category-title">Остекление</div>
-                                <div class="nav__category-content nav__category-content_horizontal">
-                                    <div class="nav__dropdown-items-group">
-                                        <a href="#" class="nav__dropdown-item">Домов и коттеджей</a>
-                                        <a href="#" class="nav__dropdown-item">Беседок, веранд и террас</a>
-                                        <a href="#" class="nav__dropdown-item">Бассейнов</a>
-                                        <a href="#" class="nav__dropdown-item">Кафе и ресторанов</a>
-                                        <a href="#" class="nav__dropdown-item">Отелей и гостиниц</a>
-                                        <a href="#" class="nav__dropdown-item">Зданий и помещений</a>
-                                    </div>
-                                    <div class="nav__dropdown-items-group">
-                                        <a href="#" class="nav__dropdown-item">Витражное</a>
-                                        <a href="#" class="nav__dropdown-item">Фасадное</a>
-                                        <a href="#" class="nav__dropdown-item">Безрамное</a>
-                                        <a href="#" class="nav__dropdown-item">Раздвижное</a>
-                                        <a href="#" class="nav__dropdown-item">Защитное</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Дизайнерские решения</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Цветные окна и двери</a>
-                                    <a href="#" class="nav__dropdown-item">Декоративные решения</a>
-                                </div>
-                            </div>
-                            <div class="nav__category">
-                                <div class="nav__category-title">Комплектующие</div>
-                                <div class="nav__category-content">
-                                    <a href="#" class="nav__dropdown-item">Стеклопакеты</a>
-                                    <a href="#" class="nav__dropdown-item">Фурнитура</a>
-                                    <a href="#" class="nav__dropdown-item">Подоконники</a>
-                                    <a href="#" class="nav__dropdown-item">Москитные сетки</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="nav__item">
-                    <div>Компания</div>
-                    <div class="nav__item-arrow"><span class="icon-arrow"></span></div>
-                    <div class="nav__dropdown">
-                        <div class="nav__dropdown-body">
-                            <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
-                                <span class="icon-arrow"></span>
-                                <span>назад</span>
-                            </div>
-                            <div class="nav__dropdown-list">
-                                <a href="#" class="nav__dropdown-item">Салон окон</a>
-                                <a href="#" class="nav__dropdown-item">Производство</a>
-                                <a href="#" class="nav__dropdown-item">Отзывы</a>
-                                <a href="#" class="nav__dropdown-item">Наши работы</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="nav__item">
-                    <div>Сервис</div>
-                    <div class="nav__item-arrow"><span class="icon-arrow"></span></div>
-                    <div class="nav__dropdown">
-                        <div class="nav__dropdown-body">
-                            <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
-                                <span class="icon-arrow"></span>
-                                <span>назад</span>
-                            </div>
-                            <div class="nav__dropdown-list">
-                                <a href="#" class="nav__dropdown-item">Кредит</a>
-                                <a href="#" class="nav__dropdown-item">Гарантия</a>
-                                <a href="#" class="nav__dropdown-item">Замер и монтаж</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <a href="#" class="nav__item">Отзывы <span class="nav__item-label">345</span></a>
-
-                <a href="{{ route('contacts') }}" class="nav__item">Контакты</a>-->
 
                 @foreach($menu_items as $menu_item)
                     @if($menu_item->page)
@@ -142,19 +18,38 @@
                             <div>{{ $menu_item->text }}</div>
                             <div class="nav__item-arrow"><span class="icon-arrow"></span></div>
                             <div class="nav__dropdown">
-                                <div class="nav__dropdown-body">
-                                    <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
-                                        <span class="icon-arrow"></span>
-                                        <span>назад</span>
-                                    </div>
-                                    <div class="nav__dropdown-list">
+                                @if($menu_item->dropdown_size == 'big')
+                                    <div class="nav__dropdown-body nav__dropdown-grid">
+                                        <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
+                                            <span class="icon-arrow"></span>
+                                            <span>назад</span>
+                                        </div>
                                         @foreach($menu_item->dropdownItems as $dropdownItem)
-                                            @if($dropdownItem->page)
-                                                <a href="{{ $dropdownItem->page->urn }}" class="nav__dropdown-item">{{ $dropdownItem->text }}</a>
-                                            @endif
+                                            <div class="nav__category">
+                                                <div class="nav__category-title">{{ $dropdownItem->text }}</div>
+                                                <div class="nav__category-content">
+                                                    @foreach($dropdownItem->children as $dropdownItemChild)
+                                                        <a href="{{ $dropdownItemChild->page->urn }}" class="nav__dropdown-item">{{ $dropdownItemChild->text }}</a>
+                                                    @endforeach
+                                                </div>
+                                            </div>
                                         @endforeach
                                     </div>
-                                </div>
+                                @else
+                                    <div class="nav__dropdown-body">
+                                        <div class="nav__dropdown-back-button hidden_md js-dropdown-hide">
+                                            <span class="icon-arrow"></span>
+                                            <span>назад</span>
+                                        </div>
+                                        <div class="nav__dropdown-list">
+                                            @foreach($menu_item->dropdownItems as $dropdownItem)
+                                                @if($dropdownItem->page)
+                                                    <a href="{{ $dropdownItem->page->urn }}" class="nav__dropdown-item">{{ $dropdownItem->text }}</a>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                     @endif
