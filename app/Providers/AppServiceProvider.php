985f0a41fa81fb13a6432e0336a1ed68dd29830d
DenisBashkirov <era-digital@yandex.ru>
2019-06-11 00:34:24 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS', true);
     }
 }
