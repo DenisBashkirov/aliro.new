2fd31cb220062b41f4fb67d34e47b1bee4a29020
DenisBashkirov <era-digital@yandex.ru>
2019-08-05 10:48:09 +0300

    Update calltracking.blade.php

--- resources/views/frontend/sources/analytics/calltracking.blade.php
+++ resources/views/frontend/sources/analytics/calltracking.blade.php
@@ -1,5 +1,5 @@
 <!-- Alloka calltracking -->
-<!--<script type="text/javascript">
+<script type="text/javascript">
     var _alloka = {
         objects: {
             'b7ec67373e59aa3d': {
@@ -18,13 +18,14 @@
 <script src="https://analytics.alloka.ru/v4/alloka.js" type="text/javascript"></script>
 
 <script src="https://analytics.alloka.ru/integrations/catch_form.js" type="text/javascript"></script>
--->
+
 
 <!-- Calltracking.ru -->
+<!--
 <script type="text/javascript">
     (function ct_load_script() {
         var ct = document.createElement('script'); ct.type = 'text/javascript';
         ct.src = document.location.protocol+'//cc.calltracking.ru/phone.e09e8.8577.async.js?nc='+Math.floor(new Date().getTime()/300000);
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ct, s);
     })();
-</script>
+</script>-->
